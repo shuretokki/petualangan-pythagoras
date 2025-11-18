248accf4fb015632419bd73fe8fd30daeae54a09
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-11-18 18:42:13 +0700

    checkpoint

--- src/views/MateriView.vue
+++ src/views/MateriView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed, useTemplateRef } from "vue";
+import { ref, computed } from "vue";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
--- src/views/test.vue
+++ src/views/test.vue
@@ -10,9 +10,11 @@ const rec = ref(
 
 const dropzoneHighlight = ref(false);
 const dropzoneOver = ref(false);
+const index = ref(0);
 
-const dragStart = (e: DragEvent, id: number) => {
+const dragStart = (_: DragEvent, id: number) => {
     console.log("item", id);
+    index.value = id;
     rec.value.forEach((r) => (r.isDragged = false));
 
     rec.value[id]!.isDragged = true;
@@ -39,15 +41,16 @@ const dragEnd = () => {
 const drop = () => {
     dropzoneHighlight.value = false;
     dropzoneOver.value = false;
+
+    rec.value = rec.value.filter((r) => r.id !== index.value);
 };
 </script>
 
 <template>
     <div class="bg-red-50 min-w-screen text-xl text-black">
         <div
-            class=""
             :class="{
-                'border bg-blue-500': dropzoneHighlight,
+                'border bg-blue-500': dropzoneHighlight && !dropzoneOver,
                 'bg-red-500': dropzoneOver,
             }"
             @dragover.prevent="dragOver"
